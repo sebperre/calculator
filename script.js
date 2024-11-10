485451e48e5a1da9056839c939e7a3341d9b0227
Sebastien <sebastienlegerperre@gmail.com>
2024-11-10 06:36:39 -0800

    added background modes, added overflow and fixed issue with spamming plus

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ const del = document.querySelector(".delete");
 const opsBtns = document.querySelectorAll(".operation")
 const equal = document.querySelector(".equal");
 
+document.body.addEventListener("click", (event) => {
+    if (event.target.tagName !== "BUTTON" && event.target.tagName !== "DIV") {
+        if (document.body.style.backgroundColor === "white") {
+            document.body.style.backgroundColor = "black";
+        } else {
+            document.body.style.backgroundColor = "white";
+        }
+    }
+});
+
 for (const btn of numberBtns) {
     btn.addEventListener("click", () => {
         number += btn.innerHTML;
@@ -50,9 +60,15 @@ del.addEventListener("click", () => {
 for (const btn of opsBtns) {
     btn.addEventListener("click", () => {
         if (inOp) {
+            if (save === "") {
+                save = "0"
+            }
+            if (number === "") {
+                number = "0"
+            }
             let result = parseOperation(btn.innerHTML, parseInt(save), parseInt(number));
             number = "";
-            output.innerHTML = number;
+            output.innerHTML = result;
             inOp = false;
         }
         else {
